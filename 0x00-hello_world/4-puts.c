a66e3586985b187dd8b8441286c83cc68dde0d63
tonnylegacy <tonnylegacy18@gmail.com>
2023-06-15 10:24:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return = 0 (success)
+ * Return=0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
