d8d5f41c8ba6066cd789a130dffd7bec14231594
tonnylegacy <tonnylegacy18@gmail.com>
2023-06-22 18:45:38 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@ void more_numbers(void)
 {
 int i, j;
 
-for (i = 0; i < 10; i++)
+for (i = 1; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j >= 9)
-_putchar('0' + (j / 10));
+if (j >= 10)
+_putchar('1');
 _putchar('0' + (j % 10));
 }
 _putchar('\n');
