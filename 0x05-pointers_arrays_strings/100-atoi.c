af6829206b1bbff1a43a2e4185729522376ef502
tonnylegacy <tonnylegacy18@gmail.com>
2023-06-26 16:45:35 -0700

    2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,41 +2,40 @@
 
 /**
  * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to be converted.
+ * @s: The string to convert.
  *
  * Return: The converted integer.
  */
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
+	unsigned int num = 0;
 	int i = 0;
+	int found_number = 0;
 
-	/* Handle the sign */
-	if (s[0] == '-')
-	{
-		sign = -1;
+	/* Handle leading whitespace */
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
 		i++;
-	}
-	else if (s[0] == '+')
-	{
-		i++;
-	}
 
-	/* Process the digits */
-	while (s[i] != '\0')
+	/* Check for sign */
+	while (s[i] == '-' || s[i] == '+')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			break;
-		}
-
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
 
-	return (sign * result);
+	/* Convert string to integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		found_number = 1;
+		i++;
+	}
+
+	/* Return 0 if no number is found */
+	if (!found_number)
+		return (0);
+
+	return (num * sign);
 }
