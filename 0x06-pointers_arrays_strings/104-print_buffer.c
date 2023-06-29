f0af2f15c207fd04fc9c2247ec8508d70226825d
tonnylegacy <tonnylegacy18@gmail.com>
2023-06-29 13:57:23 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints the content of a buffer
+ * @b: The buffer to print
+ * @size: The number of bytes to print
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+				printf("%02x", b[j]);
+			else
+				printf("  ");
+
+			if (j % 2 != 0)
+				printf(" ");
+		}
+
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
+
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+
+		printf("\n");
+	}
+}
