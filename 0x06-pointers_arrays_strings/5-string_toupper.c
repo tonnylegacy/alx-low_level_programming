48f110397e184e5bc134d899a1f34b91bacd5f2a
tonnylegacy <tonnylegacy18@gmail.com>
2023-06-29 13:06:55 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase
+ * @str: String to convert
+ *
+ * Return: Pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+			*ptr = *ptr - ('a' - 'A');
+		ptr++;
+	}
+
+	return (str);
+}
