6af4065fed637031f5f4a6a0f519696666fdcac5
tonnylegacy <tonnylegacy18@gmail.com>
2023-07-28 02:10:09 -0700

    101-hello_holberton.asm update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,9 +6,16 @@ section .text
     extern printf
 
 main:
-    push rbp
+    ; Align the stack on a 16-byte boundary
+    mov rax, rsp
+    and rax, 0xFFFFFFFFFFFFFFF0
+    mov rsp, rax
+
+    ; Prepare arguments for printf
     mov rdi, hello    ; Load the address of the hello string into rdi
     call printf       ; Call the printf function to print the string
-    add rsp, 8        ; Adjust the stack pointer to clean up the stack
+
+    ; Clean up the stack and return
+    mov rsp, rbp
     pop rbp
     ret              ; Return from the main function
