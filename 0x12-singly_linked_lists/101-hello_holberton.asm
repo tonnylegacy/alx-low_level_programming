52c8552e4a54857b94a4798a82a16152b2657f2d
tonnylegacy <tonnylegacy18@gmail.com>
2023-07-28 02:12:52 -0700

    101-hello_holberton.asm update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,18 @@
 section .data
-    hello db "Hello, Holberton", 0
+    format db "Hello, Holberton", 0
 
 section .text
     global main
     extern printf
 
 main:
-    ; Align the stack on a 16-byte boundary
-    mov rax, rsp
-    and rax, 0xFFFFFFFFFFFFFFF0
-    mov rsp, rax
-
     ; Prepare arguments for printf
-    mov rdi, hello    ; Load the address of the hello string into rdi
-    call printf       ; Call the printf function to print the string
+    mov edi, format   ; Load the address of the format string into edi
+    xor eax, eax      ; Clear eax to indicate no floating-point arguments (XMM0 not used)
+
+    ; Call the printf function to print the string
+    call printf
 
     ; Clean up the stack and return
-    mov rsp, rbp
-    pop rbp
-    ret              ; Return from the main function
+    xor eax, eax      ; Set return value to 0
+    ret               ; Return from the main function
