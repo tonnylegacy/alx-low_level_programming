1acbca2e5abdae5e5f7c6b4df4c05f76bd20a5f5
tonnylegacy <tonnylegacy18@gmail.com>
2023-08-03 11:55:42 -0700

    101-password

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit to 1 at a given index.
- * @n: A pointer to the decimal number to modify.
- * @index: The index of the bit to set (starting from 0).
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
  *
- * Return: 1 if it worked, or -1 if an error occurred.
+ * Return: 1 for success, -1 for failure
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) * 8))
-	return (-1);
-*n |= (1UL << index);
-return (1);
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
